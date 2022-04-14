692230f761b5f78f36db9b64493eb4e920408267
Sreya <sreyarootsys@gmail.com>
2022-04-14 16:28:28 +0530

    change in config>db.py

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine,MetaData
 # from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine("mysql+pymysql://root:@localhost:3306/apidb")
+engine = create_engine("mysql+pymysql://root:@localhost:3306/apidb", pool_pre_ping=True)
 meta = MetaData()
 conn = engine.connect()
 
